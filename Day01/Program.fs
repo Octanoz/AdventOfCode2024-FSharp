b4296444dd116a004f2f6b5f46d81ea5c72bc6fe
Octanoz <130052848+Octanoz@users.noreply.github.com>
2025-02-08 07:51:20 +0100

    Merged parts 1 and 2 into single projects for the first few days

---
+++ Day01/Program.fs
@@ -0,0 +1,31 @@
+﻿open System.IO
+open AdventUtilities
+
+let inputData = InputData()
+
+let filePath = inputData.GetFilePath 1 "input"
+
+let example = File.ReadAllLines filePath |> List.ofArray
+
+let parse (lines: string list) =
+    lines
+    |> List.map (fun line -> line.Split("   ") |> Seq.map int |> Seq.toList)
+    |> List.transpose
+
+let [ first; second ] = parse example
+
+let list1 = first |> List.sort
+let list2 = second |> List.sort
+
+let partOne = List.map2 (fun x y -> abs (x - y)) list1 list2 |> List.sum
+
+let numberCounts = second |> List.countBy id |> Map.ofList
+
+let counts =
+    [ for element in first -> (element, numberCounts |> Map.tryFind element |> Option.defaultValue 0) ]
+
+let partTwo = counts |> List.map (fun (a, b) -> a * b) |> List.sum
+
+printfn $"list1: {first}\nlist2: {second}"
+printfn $"Part One: {partOne}"
+printfn $"Part Two: {partTwo}"
--- Day06/Program.fs
+++ Day06/Program.fs
@@ -1,2 +1,6 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿module Program
+
+open System
+open AdventUtilities
+
+let inputData = InputData()
